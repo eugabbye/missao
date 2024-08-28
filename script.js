28e31ef3392c807d3fa5e49ff227e4bd02f6e5a9
Gabriela Buzatto <108525705+eugabbye@users.noreply.github.com>
2024-08-28 12:34:25 -0300

    s

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const perguntas = [
             },
             {
                 texto: "Azul",
-                afirmacao: " RESPOSTA ERRADA, SÓ EXISTE O ROSA. "
+                afirmacao: " Você ainda é boiola. "
             }
         ]
     },
@@ -23,7 +23,7 @@ const perguntas = [
         alternativas: [
             {
                 texto: " Macarrão com almondegas ",
-                afirmacao: " Você adora uma almondega ",
+                afirmacao: " Você adora uma almondega. ",
             },
             {
                 texto: " Sushi ",
@@ -32,15 +32,15 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Refletindo em vantagens  entre a energia solar e hidreelétrica ",
+        enunciado: "Você tem seus cadernos completos?",
         alternativas: [
             {
-                texto: "Acredita nos pontos positivos da energia solar",
-                afirmacao: "A energia solar não polui, é renovável, limpa e silenciosa, pode ser usada em áreas isoladas da rede elétrica, necessidade mínima de manutenção, muito fácil de instalar e é barato para manter"
+                texto: "Sim",
+                afirmacao: "Você é muito estudioso"
             },
             {
-                texto: "Defende a ideia nas vantagens da energia hidreelétrica",
-                afirmacao: "As principais vantagens para promover a energia hidrelétrica é a alta produção de energia dos seus projetos, por meio das barragens, e na geração elétrica sustentável por meio de uma fonte renovável, que é a água. "
+                texto: "Não",
+                afirmacao: "O Prof. Ricardo ficou triste por você não completar o caderno dele."
             }
         ]
     },
